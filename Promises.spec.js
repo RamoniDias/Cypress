fe1ae069329b9239167330536687c831a3620b1f
ramonidias <ramonidemorais@gmail.com>
2021-10-19 22:26:12 -0300

    first commit

---
+++ Promises.spec.js
@@ -0,0 +1,65 @@
+it("Sem Testes ainda, Promise", () => { });
+
+//---------------------------------------------------------
+//Exemplo: 1
+
+//const getSomething = () => 10;
+
+//const system = () => {
+//    console.log("Início");
+//    const something = getSomething();
+//    console.log("Something is " + something);
+//    console.log("Fim !!!");
+//}
+
+//system();
+
+//----------------------------------------------------------
+//Exemplo: 2
+
+// Função que demora 1000 segundos para ser executada
+
+//const getSomething = (callback) => {
+//    setTimeout(() => {
+//        callback (12);
+//    }, 1000)   
+//}
+
+// Função que chama a função a cima e da a resposta
+// só após o processamento de 1000 segundos.
+
+//const system = () => {
+//    console.log("Início");
+//    getSomething(some =>{
+//        console.log("Something is " + some);
+//       console.log("Fim !!!");
+//   });  
+//}
+//system();
+
+//----------------------------------------------------------
+//Exemplo: 3
+
+const getSomething = () => {
+   return new Promise((resolve, reject) => {
+       setTimeout(() =>{
+       resolve(12);
+    }, 1000)   
+})
+}
+
+const system = () => {
+    console.log("Início");
+    const prom = getSomething();
+    prom.then(some =>{
+        console.log("Something is " + some);
+        console.log("Fim !!!");
+   });  
+}
+
+system();
+
+
+
+
+
---
+++ basic.spec.js
@@ -0,0 +1,61 @@
+/// <reference types = "cypress" />
+
+describe('Cypress basics', () => {
+    it("Visitar um Site e Verificar o Tiítulo",() => {
+
+        // Visitar uma determinada Pagina
+        cy.visit("https://wcaquino.me/cypress/componentes.html")
+        
+       //Pegando o Título e já testando se é igual a algum nome 
+
+      // cy.title().should("equal", "Campo de Treinamento");//should testar 
+      // cy.title().should("contain", "Campo"); //Contém a palavra campo
+
+       //Fazer os dois testes de uma vez outro modo - Melhor
+       cy.title()
+         .should("equal", "Campo de Treinamento")
+         .should("contain", "Campo");
+    })
+    
+    it('Encontrar e interagir com um elemento', () => {
+
+        cy.visit("https://wcaquino.me/cypress/componentes.html");
+
+        //cy.get('#buttonSimple').click();//1 passo, "Click" no botão
+        //cy.get('#buttonSimple').should("have.value", "Obrigado!") //2 passo, teste o botão
+        
+        //Outra forma melhor de fazer, "ENCADEADO"
+
+        cy.get('#buttonSimple')// 1° Pegar o Elemento 
+          .click()             // 2° Fazer algo ou ação no Elemento interagir com elemento da tela   
+          .should("have.value", "Obrigado!");// 3° Testar o Elemento       
+    });
+});
+
+describe.only('Testar Login da TIM', () => {
+  it('Encontrar site Tim e interagir com algum Elemento', () => {
+
+    cy.visit("https://tester1.timsmart.com.br/");
+
+    // Campo Matricula
+    cy.get('#ndamatrcula-1')
+      .should("have.value","" )
+      .type("9999999", { delay: 35 }); // Escrever neste Campo ou Elemento
+    
+    // Campo Senha
+    cy.get('#senha-1')
+      .should("have.value", "")
+      .type("Trade@2020", { delay: 35 });
+
+    // Selecionar Não sou um Robô
+    cy.get('[style="width: 304px; height: 78px;"] > div > iframe')
+    //  .select() 
+   //   .click()
+
+    // Botão ENTRAR
+    cy.get('#button-35')
+      .click({ delay: 35 })
+    
+  });
+
+});
